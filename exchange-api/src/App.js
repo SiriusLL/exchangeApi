e36c3b574cc12d649ab3de90cf08e78856de40d0
SiriusLL <sirius.love.light@gmail.com>
2021-06-18 23:41:59 -0600

    depth data

--- exchange-api/src/App.js
+++ exchange-api/src/App.js
@@ -4,20 +4,38 @@ import axios from "axios";
 
 function App() {
   const [exchangeData, setExchangeData] = useState();
+  const depthParam = "?symbol=BTCUSDT";
   const burl = "https://api.binance.com";
-  const query = "/api/v3/exchangeInfo";
+  const query = `/api/v3/depth${depthParam}`;
   const url = burl + query;
 
   const getExchangeData = () => {
-    return axios.get(`${url}`).then((res) => {
+    axios.get(`${url}`).then((res) => {
       console.log("respons", res);
-      setExchangeData(JSON.stringify(res.data));
+      // setExchangeData([JSON.stringify(res.data)]);
+      setExchangeData(res.data);
     });
   };
 
+  // const lastUpdated = exchangeData.lastUpdateId;
+  // console.log("lastUpdated", lastUpdated);
+  console.log("exchangeData", exchangeData);
+  // const bids = () => {
+  //   return exchangeData.bids.map((element) => {
+  //     return <p>{element}</p>;
+  //   });
+  // };
+  const date = () => {
+    return new Date(exchangeData.lastUpdateId);
+  };
   return (
     <div className="App">
-      {exchangeData}
+      {/* {exchangeData} */}
+      {/* {exchangeData && date} */}
+      {exchangeData &&
+        exchangeData.bids.map((element) => {
+          return <p>{element}</p>;
+        })}
       <button onClick={getExchangeData}>show me the data</button>
     </div>
   );
