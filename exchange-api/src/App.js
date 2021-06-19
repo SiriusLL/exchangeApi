4291bfef3aa5d8efaefc23803fd70b8fc7f3d981
SiriusLL <sirius.love.light@gmail.com>
2021-06-18 20:13:22 -0600

    axios api request

--- exchange-api/src/App.js
+++ exchange-api/src/App.js
@@ -1,12 +1,24 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [exchangeData, setExchangeData] = useState();
+  const burl = "https://api.binance.com";
+  const query = "/api/v3/exchangeInfo";
+  const url = burl + query;
+
+  const getExchangeData = () => {
+    return axios.get(`${url}`).then((res) => {
+      console.log("respons", res);
+      setExchangeData(JSON.stringify(res.data));
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <button>show me the data</button>
+      {exchangeData}
+      <button onClick={getExchangeData}>show me the data</button>
     </div>
   );
 }
