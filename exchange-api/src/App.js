77240f97ddd91923486cd7a892085c4c2d95fd11
SiriusLL <sirius.love.light@gmail.com>
2021-06-20 23:56:32 -0600

    save

--- exchange-api/src/App.js
+++ exchange-api/src/App.js
@@ -4,11 +4,13 @@ import axios from "axios";
 import Backdrop from "./components/Backdrop";
 import Form from "./components/Form";
 import ApiV1 from "./components/ApiV1";
+import ApiV2 from "./components/ApiV2";
 
 function App() {
   return (
     <div className="App">
-      <ApiV1 />
+      {/* <ApiV1 /> */}
+      <ApiV2 />
     </div>
   );
 }
--- exchange-api/src/components/ApiV2.js
+++ exchange-api/src/components/ApiV2.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 require("dotenv").config();
+const Binance = require("node-binance-api");
+const binance = new Binance().options({
+  APIKEY: `${process.env.APIKEY_API_KEY}`,
+  APISECRET: `${process.env.APISECRET_API_KEY}`,
+});
 
-const ApiV2 = () => {
-  const Binance = require("node-binance-api");
-  const binance = new Binance().options({
-    APIKEY: `${process.env.APIKEY_API_KEY}`,
-    APISECRET: `${process.env.APISECRET_API_KEY}`,
-  });
-
+const ticker = async () => {
   let ticker = await binance.prices();
   console.info(`Price of BTC: ${ticker.BTCUSDT}`);
-  return <h1>{ticker.BTCUSDT}</h1>;
+  return ticker.BTCUSDT;
+};
+
+const ApiV2 = () => {
+  return (
+    <div>
+      <h1>{ticker}</h1>;
+    </div>
+  );
 };
 
 export default ApiV2;
