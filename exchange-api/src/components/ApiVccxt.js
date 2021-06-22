ecedcc9d52b3723c23bb9f1db498f3c1eb83df16
SiriusLL <sirius.love.light@gmail.com>
2021-06-22 00:23:22 -0600

    electron

--- exchange-api/src/components/ApiVccxt.js
+++ exchange-api/src/components/ApiVccxt.js
@@ -1,8 +1,17 @@
+"use strict";
 const ccxt = require("ccxt");
+const ccxtpro = require("ccxt.pro");
 
 const ApiVccxt = () => {
-  console.log("exchanges", ccxt.exchanges);
-  return <div>{console.log(ccxt.exchanges)}</div>;
+  // console.log("exchanges", ccxt.exchanges);
+  (async () => {
+    const exchange = new ccxtpro.binance({ enableRateLimit: true });
+    while (true) {
+      const orderbook = await exchange.watchOrderBook("ETH/BTC");
+      console.log(new Date(), orderbook["asks"][0], orderbook["bids"][0]);
+    }
+  })();
+  return <div>{hello}</div>;
 };
 
 export default ApiVccxt;
